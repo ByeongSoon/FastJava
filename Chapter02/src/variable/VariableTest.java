fffc35bf88ba9bcc93dd81b10b1c98e8d37d01cc
Byeongsoon Jang <byeongsoon@wisoft.io>
2021-01-11 21:16:17 +0900

    Add Chapter02 Test Code.

--- Chapter02/src/variable/VariableTest.java
+++ Chapter02/src/variable/VariableTest.java
@@ -1,2 +1,10 @@
-package variable;public class VariableTest {
+package variable;
+
+public class VariableTest {
+  public static void main(String[] args) {
+      int age = 28, count;
+      System.out.println(age);
+
+      int numberOfStudent; // camel notation을 주로 사
+  }
 }
