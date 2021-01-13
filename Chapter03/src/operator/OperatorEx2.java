94751884f99044d0f4699c2468410c23bd56b914
Byeongsoon Jang <byeongsoon@wisoft.io>
2021-01-13 18:54:23 +0900

    Add Chapter03 Test Code.

--- Chapter03/src/operator/OperatorEx2.java
+++ Chapter03/src/operator/OperatorEx2.java
@@ -6,5 +6,10 @@ public class OperatorEx2 {
     int num = 5%3; // 5를 3으로 나눈 나머지값을 num에 대입
     System.out.println(num);
 
+    int num1 = 10;
+    num1 += 2; // 복합 대입연산자
+
+    System.out.println(num1);
+
   }
 }
