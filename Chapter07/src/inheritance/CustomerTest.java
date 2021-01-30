d787061b732f3166771b81ede53ba9cf210df63c
Byeongsoon Jang <byeongsoon@wisoft.io>
2021-01-30 23:51:43 +0900

    Add Overriding

--- Chapter07/src/inheritance/CustomerTest.java
+++ Chapter07/src/inheritance/CustomerTest.java
@@ -4,17 +4,19 @@ public class CustomerTest {
 
   public static void main(String[] args) {
 
-    Customer customerLee = new Customer();
-    customerLee.setCustomerName("이순신");
-    customerLee.setCustomerID(10010);
+    Customer customerLee = new Customer(10010, "이순신");
+//    customerLee.setCustomerName("이순신");
+//    customerLee.setCustomerID(10010);
     customerLee.bonusPoint = 1000;
     System.out.println(customerLee.showCustomerInfo());
 
-    VIPCustomer customerKim = new VIPCustomer();
-    customerKim.setCustomerName("김유신");
-    customerKim.setCustomerID(10020);
+    VIPCustomer customerKim = new VIPCustomer(10020, "김유신");
+//    customerKim.setCustomerName("김유신");
+//    customerKim.setCustomerID(10020);
     customerKim.bonusPoint = 10000;
     System.out.println(customerKim.showCustomerInfo());
+
+    customerLee = customerKim; // 업캐스팅, 묵시적 형변환, 하위 클래스 타입은 상위 클래스로 자동 형변환
 
   }
 
